34f3bd74fa6a4bb09807995cc5a0031466774986
smbanx <smbanx@gmail.com>
2024-11-21 18:56:57 -0800

    working trainer

---
+++ dataset.py
@@ -0,0 +1,51 @@
+import kagglehub
+import numpy as np
+import pandas as pd
+import sklearn
+import torch
+
+from sklearn.model_selection import train_test_split
+from torchvision import datasets
+from torchvision.transforms import v2
+from torch.utils.data import DataLoader, random_split, Subset
+
+path = kagglehub.dataset_download("kmader/skin-cancer-mnist-ham10000")
+
+train_transform = v2.Compose([
+    v2.Resize((224, 224)),  # Resize to 224x224
+    v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]), # ToTensor()
+    # v2.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])  # Normalize
+])
+
+val_transform = v2.Compose([
+    v2.Resize((224, 224)),
+    v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]) # ToTensor()
+])
+
+test_transform = v2.Compose([
+    v2.Resize((224, 224)),
+    v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True)]) # ToTensor()
+])
+
+train_dataset = datasets.ImageFolder(root=path, transform=train_transform)
+val_dataset = datasets.ImageFolder(root=path, transform=val_transform)
+test_dataset = datasets.ImageFolder(root=path, transform=test_transform)
+
+indices = [i for i in range(len(train_dataset))]
+
+train_indices, indices_ = sklearn.model_selection.train_test_split(indices, test_size=0.30, random_state=42)
+val_indices, test_indices = sklearn.model_selection.train_test_split(indices_, test_size=0.50, random_state=42)
+
+train_set = Subset(train_dataset, train_indices)
+val_set = Subset(val_dataset, val_indices)
+test_set = Subset(test_dataset, test_indices)
+
+# train_set, val_set, test_set = random_split(dataset, [0.7, 0.15, 0.15])
+# train_set.dataset.transform = train_transform
+# val_set.dataset.transform = val_transform
+
+train_loader = DataLoader(train_set, batch_size=32, shuffle=True)
+val_loader = DataLoader(val_set, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_set, batch_size=32, shuffle=True)
+
+print("Path to dataset files:", path)
---
+++ trainer.py
@@ -0,0 +1,173 @@
+import argparse
+import os
+import shutil
+import time
+import timm
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+from dataset import train_set, val_set, test_set, train_loader, val_loader, test_loader
+from timm.models import VisionTransformer
+from timm.models import Bottleneck, ResNetV2, resnetv2
+from timm.models.resnet import resnet50
+from timm.utils import AverageMeter
+from tqdm import tqdm
+
+best_acc = 0
+
+parser = argparse.ArgumentParser(description='PyTorch MixMatch Training')
+# Optimization options
+parser.add_argument('--epochs', default=1024, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--lr', '--learning-rate', default=0.002, type=float,
+                    metavar='LR', help='initial learning rate')
+parser.add_argument('--out', default='result',
+                        help='Directory to output the result')
+parser.add_argument('--train-iteration', type=int, default=1024,
+                        help='Number of iteration per epoch')
+
+args = parser.parse_args()
+state = {k: v for k, v in args._get_kwargs()}
+
+def main():
+    global best_acc
+
+    model = VisionTransformer(num_classes=4)
+    model = model.cuda()
+
+    ema_model = VisionTransformer(num_classes=4)
+    for param in ema_model.parameters():
+        param.detach_()
+    ema_model = ema_model.cuda()
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, momentum=0.9)
+
+    start_epoch = 0
+
+    step = 0
+    test_accs = []
+    # Train and val
+    for epoch in range(start_epoch, args.epochs):
+        print('\nEpoch: [%d | %d] LR: %f' % (epoch + 1, args.epochs, state['lr']))
+
+        train_loss = train(train_loader, model, optimizer, criterion)
+        # _, train_acc = validate(train_loader, ema_model, criterion)
+        val_loss, val_acc = validate(val_loader, ema_model, criterion)
+        test_loss, test_acc = validate(test_loader, ema_model, criterion)
+        print('Validation Loss: {:.2f}'.format(val_loss))
+        print('Validation Top-1 Accuracy: {:.2f}%'.format(val_acc))
+        print('Test Top-1 Accuracy: {:.2f}%'.format(test_acc))
+        step = args.train_iteration * (epoch + 1)
+
+        # save model
+        is_best = val_acc > best_acc
+        best_acc = max(val_acc, best_acc)
+        save_checkpoint({
+            'epoch': epoch + 1,
+            'state_dict': model.state_dict(),
+            'ema_state_dict': ema_model.state_dict(),
+            'acc': val_acc,
+            'best_acc': best_acc,
+            'optimizer': optimizer.state_dict(),
+        }, is_best)
+        test_accs.append(test_acc)
+
+def train(train_loader, model, optimizer, criterion):
+
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    end = time.time()
+
+    # train_iter = iter(train_loader)
+    model.train()
+    # for batch in tqdm(train_loader, desc="Training", leave=True):
+    for inputs_x, targets_x in tqdm(train_loader, desc="Training", leave=True):
+        # inputs_x, targets_x = next(train_iter)
+        # inputs_x, targets_x = batch
+
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        batch_size = inputs_x.size(0)
+
+        inputs_x, targets_x = inputs_x.cuda(), targets_x.cuda(non_blocking=True)
+
+        logits = model(inputs_x)
+
+        loss = criterion(logits, targets_x)
+
+        # record loss
+        losses.update(loss.item(), inputs_x.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+    return losses.avg
+
+def validate(val_loader, model, criterion):
+
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top3 = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+
+    end = time.time()
+    with torch.no_grad():
+        # for batch_idx, (inputs, targets) in enumerate(val_loader):
+        for inputs, targets in tqdm(val_loader, desc="Evaluating", leave=True):
+            # measure data loading time
+            data_time.update(time.time() - end)
+
+            inputs, targets = inputs.cuda(), targets.cuda(non_blocking=True)
+            # compute output
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+
+            # measure accuracy and record loss
+            prec1 = accuracy(outputs, targets, topk=(1,))[0]
+            losses.update(loss.item(), inputs.size(0))
+            top1.update(prec1.item(), inputs.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+    return (losses.avg, top1.avg)
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+def save_checkpoint(state, is_best, checkpoint=args.out, filename='checkpoint.pth.tar'):
+    if not os.path.exists(checkpoint):
+        os.makedirs(checkpoint)
+    filepath = os.path.join(checkpoint, filename)
+    torch.save(state, filepath)
+    if is_best:
+        shutil.copyfile(filepath, os.path.join(checkpoint, 'model_best.pth.tar'))
+
+if __name__ == '__main__':
+    main()
