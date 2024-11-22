83daf169fe6b6cf1861d9ce3502e94a4db55373d
smbanx <smbanx@gmail.com>
2024-11-22 15:51:45 -0800

    fixed data load + classification to benign (0) vs. malignant (1)

--- dataset.py
+++ dataset.py
@@ -1,15 +1,60 @@
 import kagglehub
 import numpy as np
+import os
 import pandas as pd
+import shutil
 import sklearn
 import torch
 
+from pathlib import Path
 from sklearn.model_selection import train_test_split
 from torchvision import datasets
 from torchvision.transforms import v2
-from torch.utils.data import DataLoader, random_split, Subset
+from torch.utils.data import DataLoader, Dataset, random_split, Subset
+
+
+class HAM10000Dataset(Subset):
+    def __init__(self, dataset, indices, metadata=None):
+        super().__init__(dataset, indices)
+        self.metadata = metadata
+
+    def __getitem__(self, idx):
+        image, _ = super().__getitem__(idx)
+        row = self.metadata.iloc[self.indices[idx]]
+        label = row['class_idx']
+        return image, label
+
 
 path = kagglehub.dataset_download("kmader/skin-cancer-mnist-ham10000")
+
+metadata = pd.read_csv(f"{path}/HAM10000_metadata.csv")
+benign_classes = ["nv", "bkl", "df", "vasc"]
+malignant_classes = ["mel", "bcc", "akiec"]
+
+class_mapping = {cls: 0 if cls in benign_classes else 1 for cls in benign_classes + malignant_classes}
+metadata['class_idx'] = metadata['dx'].map(class_mapping)
+
+data_path = path + '/data'
+path_part1 = path + '/ham10000_images_part_1'
+path_part2 = path + '/ham10000_images_part_2'
+
+source_dirs = [path_part1, path_part2]
+if not os.path.exists(data_path):
+    os.makedirs(data_path)
+    os.makedirs(f"{data_path}/0")
+    os.makedirs(f"{data_path}/1")
+    for _, row in metadata.iterrows():
+        image_id = row['image_id'] + ".jpg"
+        class_dir = f"{data_path}/{row['class_idx']}"  # 0 or 1
+
+        # Locate the image in the source directories
+        for src_dir in source_dirs:
+            src_path = os.path.join(src_dir, image_id)
+            if os.path.exists(src_path):
+                shutil.copy(src_path, os.path.join(class_dir, image_id))
+                break
+
+path = data_path
 
 train_transform = v2.Compose([
     v2.Resize((224, 224)),  # Resize to 224x224
@@ -38,6 +83,9 @@ indices = [i for i in range(len(train_dataset))]
 train_indices, indices_ = sklearn.model_selection.train_test_split(indices, test_size=0.30, random_state=42)
 val_indices, test_indices = sklearn.model_selection.train_test_split(indices_, test_size=0.50, random_state=42)
 
+# train_set = HAM10000Dataset(train_dataset, train_indices, metadata)
+# val_set = HAM10000Dataset(val_dataset, val_indices, metadata)
+# test_set = HAM10000Dataset(test_dataset, test_indices, metadata)
 train_set = Subset(train_dataset, train_indices)
 val_set = Subset(val_dataset, val_indices)
 test_set = Subset(test_dataset, test_indices)
--- trainer.py
+++ trainer.py
@@ -7,14 +7,15 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from dataset import train_set, val_set, test_set, train_loader, val_loader, test_loader
-from timm.models import VisionTransformer
-from timm.models import Bottleneck, ResNetV2, resnetv2
+from dataset import train_loader, val_loader, test_loader
+from timm.models import VisionTransformer, Bottleneck, ResNetV2, resnetv2
 from timm.models.resnet import resnet50
 from timm.utils import AverageMeter
 from tqdm import tqdm
 
 best_acc = 0
+best_loss = float('inf')
+best_epoch = -1
 
 parser = argparse.ArgumentParser(description='PyTorch MixMatch Training')
 # Optimization options
@@ -24,29 +25,43 @@ parser.add_argument('--lr', '--learning-rate', default=1.0e-4, type=float,
                     metavar='LR', help='initial learning rate')
 parser.add_argument('--out', default='result',
                         help='Directory to output the result')
-parser.add_argument('--train-iteration', type=int, default=1024,
-                        help='Number of iteration per epoch')
 
 args = parser.parse_args()
 state = {k: v for k, v in args._get_kwargs()}
 
+def initialize_weights(model):
+    for module in model.modules():
+        if isinstance(module, nn.Linear):
+            nn.init.xavier_uniform_(module.weight)
+            if module.bias is not None:
+                nn.init.constant_(module.bias, 0)
+        elif isinstance(module, nn.Conv2d):
+            nn.init.kaiming_normal_(module.weight, nonlinearity='relu')
+            if module.bias is not None:
+                nn.init.constant_(module.bias, 0)
+        elif isinstance(module, nn.LayerNorm):
+            nn.init.constant_(module.bias, 0)
+            nn.init.constant_(module.weight, 1.0)
+        elif isinstance(module, nn.Embedding):
+            nn.init.normal_(module.weight, mean=0, std=0.02)
+
+
 def main():
     global best_acc
+    global best_loss
+    global best_epoch
 
-    model = VisionTransformer(num_classes=4)
+    # model = VisionTransformer(num_classes=4)
+    model = timm.create_model('vit_base_patch16_224', num_classes=2, pretrained=False)
+    initialize_weights(model)
+    torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0) # gradient clipping
     model = model.cuda()
 
-    # ema_model = VisionTransformer(num_classes=4)
-    # for param in ema_model.parameters():
-    #     param.detach_()
-    # ema_model = ema_model.cuda()
-
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=1e-4)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=1.0e-6)
 
     start_epoch = 0
 
-    step = 0
     val_accs = []
     val_losses = []
     test_accs = []
@@ -56,23 +71,24 @@ def main():
         print('\nEpoch: [%d | %d] LR: %f' % (epoch + 1, args.epochs, state['lr']))
 
         train_loss = train(train_loader, model, optimizer, criterion)
-        # _, train_acc = validate(train_loader, ema_model, criterion)
-        # val_loss, val_acc = validate(val_loader, ema_model, criterion)
-        # test_loss, test_acc = validate(test_loader, ema_model, criterion)
+        # _, train_acc = validate(train_loader, model, criterion)
         val_loss, val_acc = validate(val_loader, model, criterion)
         test_loss, test_acc = validate(test_loader, model, criterion)
         print('Validation Loss: {:.2f}'.format(val_loss))
         print('Validation Top-1 Accuracy: {:.2f}%'.format(val_acc))
         print('Test Top-1 Accuracy: {:.2f}%'.format(test_acc))
-        step = args.train_iteration * (epoch + 1)
 
         # save model
-        is_best = val_acc > best_acc
-        best_acc = max(val_acc, best_acc)
+        # is_best = val_acc > best_acc
+        # best_acc = max(val_acc, best_acc)
+        is_best = val_loss < best_loss
+        best_loss = min(val_loss, best_loss)
+        if is_best:
+            best_acc = val_acc
+            best_epoch = epoch
         save_checkpoint({
             'epoch': epoch + 1,
             'state_dict': model.state_dict(),
-            # 'ema_state_dict': ema_model.state_dict(),
             'acc': val_acc,
             'best_acc': best_acc,
             'optimizer': optimizer.state_dict(),
@@ -81,6 +97,9 @@ def main():
         val_losses.append(val_loss)
         test_accs.append(test_acc)
         test_losses.append(test_loss)
+        print('Best Loss: {:.2f}'.format(best_loss))
+        print('Best Accuracy: {:.2f}%'.format(best_acc))
+        print('Best Epoch: {:d}'.format(best_epoch))
 
 def train(train_loader, model, optimizer, criterion):
 
@@ -89,19 +108,13 @@ def train(train_loader, model, optimizer, criterion):
     losses = AverageMeter()
     end = time.time()
 
-    # train_iter = iter(train_loader)
     model.train()
-    # for batch in tqdm(train_loader, desc="Training", leave=True):
     for inputs_x, targets_x in tqdm(train_loader, desc="Training", leave=True):
-        # inputs_x, targets_x = next(train_iter)
-        # inputs_x, targets_x = batch
         if torch.isnan(inputs_x).any() or torch.isinf(inputs_x).any():
             print("Inputs contain NaN or Inf after normalization!")
 
         # measure data loading time
         data_time.update(time.time() - end)
-
-        batch_size = inputs_x.size(0)
 
         inputs_x, targets_x = inputs_x.cuda(), targets_x.cuda(non_blocking=True)
 
@@ -129,14 +142,13 @@ def validate(val_loader, model, criterion):
     data_time = AverageMeter()
     losses = AverageMeter()
     top1 = AverageMeter()
-    top3 = AverageMeter()
+    # top3 = AverageMeter()
 
     # switch to evaluate mode
     model.eval()
 
     end = time.time()
     with torch.no_grad():
-        # for batch_idx, (inputs, targets) in enumerate(val_loader):
         for inputs, targets in tqdm(val_loader, desc="Evaluating", leave=True):
             if torch.isnan(inputs).any() or torch.isinf(inputs).any():
                 print("Inputs contain NaN or Inf after normalization!")
